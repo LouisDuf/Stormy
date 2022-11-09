57fbc7181d5853922ba8b4a069728eff5fa5df6a
louwar <loup6378@gmail.com>
2022-11-09 09:52:36 +0100

    Add firebase Todo Fix

--- Application/src/app/app.module.ts
+++ Application/src/app/app.module.ts
@@ -7,9 +7,35 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYhgbFPgYbJLz_A3BsomGrWTELxOIiaYc",
+  authDomain: "storymy-41c7e.firebaseapp.com",
+  projectId: "storymy-41c7e",
+  storageBucket: "storymy-41c7e.appspot.com",
+  messagingSenderId: "462763749240",
+  appId: "1:462763749240:web:55e15e0214c0b67770628d"
+};
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- Application/src/app/components/content-home/content-home.component.ts
+++ Application/src/app/components/content-home/content-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-content-home',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-home.component.scss'],
 })
 export class ContentHomeComponent implements OnInit {
+  
+  nomTache: string = "RDV sorcière";
 
-  constructor() { }
+  
+  constructor(public afDB: AngularFireDatabase) { }
 
   ngOnInit() {}
+
+  add() {
+    this.afDB.list('Event').push(this.nomTache)
+  }
 
 }
