6081ec58ceda375ef05b950d8def3ee561387cca
NoaSlld <102046195+NoaSlld@users.noreply.github.com>
2022-11-25 14:50:26 +0100

    tentative popup

--- Application/src/app/home/home.page.ts
+++ Application/src/app/home/home.page.ts
@@ -10,3 +10,35 @@ export class HomePage {
   constructor() {}
 
 }
+
+import { AlertController } from '@ionic/angular';
+
+export class ExampleComponent {
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Ajouter un DS',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Nom de la matière',
+        },
+        {
+          //placeholder: 'Nickname (max 8 characters)',
+          //attributes: {
+          //  maxlength: 8,
+          //},
+        },
+        {
+          //type: 'number',
+          placeholder: 'Heure',
+          min: 1,
+          max: 100,
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+}
