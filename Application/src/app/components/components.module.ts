4d429946ce199f37a9391eae0cacea5574aa88ce
louwar <loup6378@gmail.com>
2022-11-28 11:09:47 +0100

    Update todo list & add components

--- Application/src/app/components/components.module.ts
+++ Application/src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { CardAjoutComponent } from './card-ajout/card-ajout.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
-  declarations: [CardAjoutComponent],
-  exports: [CardAjoutComponent]
+  declarations: [CardAjoutComponent,TaskComponent],
+  exports: [CardAjoutComponent,TaskComponent]
 })
 export class ComponentsModule {}
